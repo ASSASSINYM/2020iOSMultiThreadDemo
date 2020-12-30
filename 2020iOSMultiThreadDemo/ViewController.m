9128a84d9982839f4d59c1f556030e457e7758d6
ASSASSINYM <darlingiloveu@sina.com>
2020-12-30 17:05:21 +0800

    cocoapods 添加 AFNetworking

--- 2020iOSMultiThreadDemo/ViewController.m
+++ 2020iOSMultiThreadDemo/ViewController.m
@@ -16,6 +16,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    BOOL ss = [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"cydia://"]];
+    
 }
 
 
